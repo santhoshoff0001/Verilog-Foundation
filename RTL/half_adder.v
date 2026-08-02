fecb116648668e19ab9a4e881b90db72cbf88166
Santhosh <santhoshofficial0001@gmail.com>
2026-08-02 14:41:46 +0530

    Removed Vivado auto-generated boilerplate

--- RTL/half_adder.v
+++ RTL/half_adder.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/10/2026 11:51:40 AM
-// Design Name: 
-// Module Name: half_adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 module Half_adder(input a,b,output reg sum,carry);
   
   always@(a,b)
--- Testbench/tb_half_adder.v
+++ Testbench/tb_half_adder.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/10/2026 11:55:20 AM
-// Design Name: 
-// Module Name: tb_half_adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module tb_half_adder;
   
